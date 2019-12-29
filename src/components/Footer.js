97d4751ae16cf5249fc5802bed6c1b93bd15c231
Aida Manna <aidamanna@gmail.com>
2019-12-29 23:30:56 +0100

    Improve page layout and add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <p>@aidamanna</p>
+  </footer>
+)
+
+export default Footer
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Navbar from './Navbar'
+import Footer from './Footer'
 import List from './List'
 import Detail from './Detail'
 
 const Root = () => (
   <BrowserRouter>
-    <div>
+    <div className="page-container">
       <Navbar/>
-      <Route exact path="/" component={List}/>
-      <Route exact path="/:id" component={Detail}/>
+      <div className="content-wrapper">
+        <Route exact path="/" component={List}/>
+        <Route exact path="/:id" component={Detail}/>
+      </div>
+      <Footer/>
     </div>
   </BrowserRouter>
 )
