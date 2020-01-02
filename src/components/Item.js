1cf74f4f00adae1884f7755f4c5255d6bc7156b0
Aida Manna <aidamanna@gmail.com>
2020-01-02 19:50:47 +0100

    Add edit and delete buttons in devices list

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Item = ({ data }) => (
   <li className="list-item" >
     <Link to={`/${data.imei}`}>
       <strong>{data.model}</strong> - {data.vendor} {data.operatingSystem} {data.operatingSystemVersion}
     </Link>
+    <div className="icon-buttons-wrapper">
+      <button className="icon-button">
+        <FontAwesomeIcon icon={faPen}/>
+      </button>
+      <button className="icon-button">
+        <FontAwesomeIcon icon={faTrashAlt}/>
+      </button>
+    </div>
   </li>
 )
 
