973b8a1ebec373316eaed52e0106eab6891c1ccf
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2020-01-10 17:26:31 +0100

    Fix prop type error

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 
 const IconButton = (props) => (
   <button className={'icon-btn-' + props.theme} onClick={props.onClick}>
-      {props.children}
+    {props.children}
   </button>
 )
 
 IconButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   theme: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired
+  children: PropTypes.object.isRequired
 }
 
 export default React.memo(IconButton)
