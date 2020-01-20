9e62129c4f09a563d22c3f8e3afe2ad7aece83dd
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2020-01-20 10:16:55 +0100

    Show error message if credentials are invalid

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -6,9 +6,10 @@ class LogIn extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isSending: false,
       email: '',
-      password: ''
+      password: '',
+      isSending: false,
+      showErrorMessage: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -29,7 +30,7 @@ class LogIn extends Component {
     this.setState({ isSending: true })
     logIn(email, password)
       .then(() => this.props.history.push('/devices'))
-      .catch(() => this.setState({ isSending: false }))
+      .catch(() => this.setState({ isSending: false, showErrorMessage: true }))
   }
 
   componentDidMount () {
@@ -37,7 +38,7 @@ class LogIn extends Component {
   }
 
   render () {
-    const { email, password, isSending } = this.state
+    const { email, password, isSending, showErrorMessage } = this.state
 
     return (<div className="login-container">
       <div className="login-wrapper">
@@ -49,6 +50,7 @@ class LogIn extends Component {
           <form className="login-content" >
             <input type="text" value={email} placeholder="Email" onChange={this.handleChange('email')} ref={(input) => { this.emailInput = input }}/>
             <input type="password" value={password} placeholder="Password" onChange={this.handleChange('password')}/>
+            { showErrorMessage && <p className="error-msg">Invalid username or password</p> }
           </form>
           <footer className="login-footer">
             <input type="submit" value="Log in" onClick={this.handleSubmit} disabled={isSending} className="btn-default-long"/>
