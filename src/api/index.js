1d2317d000f5fb1c386f636374a10994dc4bebf1
Aida Manna <aidamanna@gmail.com>
2020-04-27 08:45:21 +0200

    Update api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8080/'
+const url = 'http://api.devicenator.com/'
 
 export const listDevices = () => {
   return axios.get(url + 'devices', {
