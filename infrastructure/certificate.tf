ff9f08cd6bf434204fa7da503f68167c5a9d0b4d
aidamanna <aida.mannaloyarte@thoughtworks.com>
2020-07-09 14:24:06 +0200

    Create the certificate in North Virginia

--- infrastructure/certificate.tf
+++ infrastructure/certificate.tf
@@ -1,6 +1,13 @@
+provider "aws" {
+  profile = "devicenator"
+  region = "us-east-1"
+  alias = "virginia"
+}
+
 resource "aws_acm_certificate" "certificate" {
   domain_name = local.domain_name
   validation_method = "DNS"
+  provider = aws.virginia
 
   tags = {
     Name = "devicenator-certificate"
@@ -18,4 +25,5 @@ resource "aws_route53_record" "certificate_validation" {
 resource "aws_acm_certificate_validation" "certificate_validation" {
   certificate_arn = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [aws_route53_record.certificate_validation.fqdn]
+  provider = aws.virginia
 }
