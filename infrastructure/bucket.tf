03d547e171942fee406b3d8a7d68a6dfab1b0037
aidamanna <aida.mannaloyarte@thoughtworks.com>
2020-07-14 11:35:08 +0200

    Restrict access to S3 bucket to CloudFront only

--- infrastructure/bucket.tf
+++ infrastructure/bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "devicenator-ui" {
   bucket = local.domain_name
-  acl = "public-read"
+  acl = "private"
 
   website {
     index_document = "index.html"
@@ -15,19 +15,23 @@ resource "aws_s3_bucket_policy" "s3-bucket-policy" {
 
 data "aws_iam_policy_document" "s3-bucket-policy-document" {
   statement {
-    sid = "PublicReadGetObject"
+    sid = "CloudFrontListBucket"
     effect = "Allow"
     principals {
-      identifiers = [
-        "*"
-      ]
-      type = "*"
+      type = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
     }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "arn:aws:s3:::${local.domain_name}/*"
-    ]
+    actions = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${local.domain_name}"]
+  }
+  statement {
+    sid = "CloudFrontGetObject"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+    actions = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${local.domain_name}/*"]
   }
 }
